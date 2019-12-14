e33224cb6a0c5f2c5341c1d06bf6bc282ca6e542
Zykin S. Dmitry <zykin.dmitry@gmail.com>
2019-12-14 15:13:41 +0300

    Добавил api +ресурс /api/permissions + ioc

--- DevFactoryZ.CharityCRM.UI.Web/Startup.cs
+++ DevFactoryZ.CharityCRM.UI.Web/Startup.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using DevFactoryZ.CharityCRM.Ioc;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,10 +9,20 @@ namespace DevFactoryZ.CharityCRM.UI.Web
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddSingleton(Configuration);
+            services.WithDataAccessComponents("local");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +37,7 @@ namespace DevFactoryZ.CharityCRM.UI.Web
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
