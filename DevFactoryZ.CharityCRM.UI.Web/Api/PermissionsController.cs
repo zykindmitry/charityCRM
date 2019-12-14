5f2a19c2e9d59777b6ccb42a9a8437457a6a9d16
Zykin S. Dmitry <zykin.dmitry@gmail.com>
2019-12-14 15:13:41 +0300

    Добавил api +ресурс /api/permissions + ioc

---
+++ DevFactoryZ.CharityCRM.UI.Web/Api/PermissionsController.cs
@@ -0,0 +1,25 @@
+﻿using DevFactoryZ.CharityCRM.Persistence;
+using DevFactoryZ.CharityCRM.UI.Web.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace DevFactoryZ.CharityCRM.UI.Web.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PermissionsController : ControllerBase
+    {
+        private readonly IPermissionRepository repository;
+
+        public PermissionsController(IPermissionRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        public PermissionViewModel[] Get()
+        {
+            return repository.GetAll().Select(model => new PermissionViewModel(model)).ToArray();
+        }
+    }
+}
---
+++ DevFactoryZ.CharityCRM.UI.Web/Api/ViewModels/PermissionViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace DevFactoryZ.CharityCRM.UI.Web.Api.Models
+{
+    public class PermissionViewModel
+    {
+        public PermissionViewModel()
+        {
+        }
+
+        public PermissionViewModel(Permission model)
+        {
+            Id = model.Id;
+            Name = model.Name;
+            Description = model.Description;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
