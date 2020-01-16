ef4db616e242763c652438d30bc65ebdb3d67951
Andrey Maykut <maykout@yandex.ru>
2020-01-16 23:49:04 +0300

    Изменения в методы создания/обновления AccountsController

--- DevFactoryZ.CharityCRM.UI.Web/Api/PermissionsController.cs
+++ DevFactoryZ.CharityCRM.UI.Web/Api/PermissionsController.cs
@@ -2,37 +2,45 @@
 using DevFactoryZ.CharityCRM.UI.Web.Api.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace DevFactoryZ.CharityCRM.UI.Web.Api
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PermissionsController : ApiController<IPermissionService>
+    public class AccountsController : ApiController<IAccountService>
     {
-        public PermissionsController(IPermissionService service) : base(service)
-        { 
+        public AccountsController(IAccountService servce) : base(servce)
+        {
+                
         }
 
+        // GET: api/<controller>
         [HttpGet]
         [Authorize]
-        public ActionResult<PermissionListModel[]> Get()
+        public ActionResult<AccountListModel[]> Get()
         {
             return GetResultWithErrorHandling(
-               service => service.GetAll().Select(model => new PermissionListModel(model)).ToArray());
+                service => service.GetAll().Select(model => new AccountListModel(model)).ToArray());
         }
 
+        // GET api/<controller>/5
         [HttpGet("{id}")]
         [Authorize]
-        public ActionResult<PermissionListModel> Get(int id)
+        public ActionResult<AccountListModel> Get(int id)
         {
             return GetResultWithErrorHandling(
-                service => new PermissionListModel(service.GetById(id)));
+                service => new AccountListModel(service.GetById(id)));
         }
 
+        // POST api/<controller>
         [HttpPost]
-        [Authorize]
-        public ActionResult<PermissionListModel> Post([FromBody]PermissionModel viewModel)
+        public ActionResult<AccountListModel> Post([FromBody]AccountModel viewModel)
         {
             if (viewModel == null)
             {
@@ -42,14 +50,15 @@ namespace DevFactoryZ.CharityCRM.UI.Web.Api
             return GetResultWithErrorHandling(
                 service =>
                 {
-                    var model = service.Create(viewModel.ToDto());
-                    return new PermissionListModel(model);
+                    var model = service.Create(viewModel.ToDtoForCreate());
+                    return new AccountListModel(model);
                 });
         }
 
+        // PUT api/<controller>/5
         [HttpPut("{id}")]
         [Authorize]
-        public IActionResult Put(int id, [FromBody]PermissionModel viewModel)
+        public IActionResult Put(int id, [FromBody]AccountModel viewModel)
         {
             if (viewModel == null)
             {
@@ -57,9 +66,11 @@ namespace DevFactoryZ.CharityCRM.UI.Web.Api
             }
 
             return ExecuteWithErrorHandling(
-                service => service.Update(id, viewModel.ToDto()));                
+                service => service.Update(id, viewModel.ToDtoForUpdate())
+                );
         }
 
+        // DELETE api/<controller>/5
         [HttpDelete("{id}")]
         [Authorize]
         public IActionResult Delete(int id)
