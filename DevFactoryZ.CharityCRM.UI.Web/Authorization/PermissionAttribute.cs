57cf5e886fc312db7db575932dcda080b9972315
Andrey Maykut <maykout@yandex.ru>
2020-01-28 01:30:59 +0300

    Аттрибут PermissionAttribute

---
+++ DevFactoryZ.CharityCRM.UI.Web/Authorization/PermissionAttribute.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DevFactoryZ.CharityCRM.UI.Web.Authorization
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class PermssionAttribute : AuthorizeAttribute, IAuthorizationFilter
+    {
+        public string Permissions { get; set; }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (string.IsNullOrEmpty(Permissions))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            var userName = context.HttpContext.User.Identity.Name;
+            IEnumerable<Permission> userPermissions = null; //
+            var requiredPermissions = Permissions.Split(",");
+
+            foreach (var permission in requiredPermissions)
+            {
+                if (userPermissions.Select(p => p.Name).ToList().Contains(permission))
+                {
+                    return;
+                }
+            }
+
+            context.Result = new UnauthorizedResult();
+            return;
+        }
+    }
+}
